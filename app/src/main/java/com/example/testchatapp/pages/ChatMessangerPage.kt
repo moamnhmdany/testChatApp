369076eb7b3a8f3bc80d1740e5e209b90ee936c4
moamnhmdany <moamnhmdany2@gmail.com>
2023-08-19 22:28:23 +0300

    add chat massenger page

---
+++ app/src/main/java/com/example/testchatapp/pages/ChatMessangerPage.kt
@@ -0,0 +1,12 @@
+package com.example.testchatapp.pages
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.testchatapp.R
+
+class ChatMessangerPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_messanger_page)
+    }
+}
